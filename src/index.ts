385ce00894cd523c089145c36bd4d5f9225cec46
Hayden-Althoff <hayden.althoff@smail.astate.edu>
2023-01-20 18:17:43 -0600

    Updates to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-const array1: Array<number> = [4, 5, 23, 18, 9, -5];
-const array2: Array<number> = [18, 74, 88, 3, 7, 44];
+const array1: Array<number> = [4, 5, 23, 18, 9, -5, 77, 84];
+const array2: Array<number> = [18, 74, 88, 3, 7];
 
 const mergedArray: Array<number> = merge(array1, array2);
 console.log(mergedArray);
+
 
 
 //wordle
@@ -38,8 +39,6 @@ function checkWord( secret: string, guess: string ): string{
   return result;
 }
 
-console.log('test');
-
 
 
 //candidate
@@ -53,6 +52,8 @@ let edUnderwood: Candidate = { name: "Edward Underwood", votes: [192, 147, 186, 
 let roseOlsen: Candidate = { name: "Rose Olsen", votes: [48, 90, 12, 21, 13], funding: 78889263};
 let leonardWillis: Candidate = { name: "Leonard Willis", votes: [206, 312, 121, 408, 382], funding: 36070689};
 let nateTaylor: Candidate = { name: "Nathaniel Taylor", votes: [37, 21, 38, 39, 29], funding: 6317921937};
+
+let candidates: Array<Candidate> = [edUnderwood, roseOlsen, leonardWillis, nateTaylor];
 
 //I couldnt figure out the syntax for adding member functions or I would have included all of this in the Candidate type
 let euTotal: number = getTotal( edUnderwood.votes );
@@ -69,11 +70,10 @@ let roSpent: number = roseOlsen.funding / roTotal;
 let lwSpent: number = leonardWillis.funding / lwTotal;
 let ntSpent: number = nateTaylor.funding / ntTotal;
 
+printTotals(candidates);
 
-console.log(`Edward Underwood -- ${euTotal} -- ${euPercentage}`);
-console.log(`Rose Olsen -- ${roTotal} -- ${roPercentage}`);
-console.log(`Leonard Willis -- ${lwTotal} -- ${lwPercentage}`);
-console.log(`Nathaniel Taylor -- ${ntTotal} -- ${ntPercentage}`);
+
+
 
 function getTotal( arry: Array<number>): number{
   let total: number = 0;
@@ -81,6 +81,20 @@ function getTotal( arry: Array<number>): number{
       total = total + arry[i];
     }
     return total;
+}
+
+function printTotals(candidates: Array<Candidate>): void{
+  let totalVotes: number = 0;
+  for (let i: number = 0; i < candidates.length; i +=1){
+    totalVotes = totalVotes + getTotal(candidates[i].votes);
+  }
+  for (let i: number = 0; i < candidates.length; i +=1){
+    let total: number = getTotal(candidates[i].votes);
+    console.log(`${candidates[i].name} -- ${total} -- ${((getTotal(candidates[i].votes) / totalVotes) * 100).toFixed(2)}%`);
+  }
+
+
+
 }
 
 
@@ -95,9 +109,16 @@ function getTotal( arry: Array<number>): number{
 function merge (arr1: Array<number>, arr2: Array<number>): Array<number>{
   let arr3: Array<number> = [];
   let position: number = 0;
-    for(let i: number = 0; i < arr1.length; i += 2){
+    for(let i: number = 0; i < (arr1.length + arr2.length); i += 2){
       arr3[i] = arr1[position];
-      arr3[i + 1] = arr2[position];
+      if(i < (arr2.length * 2)){
+        arr3[i + 1] = arr2[position];
+      }
+      //sets the i variable back on track with the bigger list.
+      else {
+        i -= 1;
+      }
+
       position += 1;
     }
     return arr3;
